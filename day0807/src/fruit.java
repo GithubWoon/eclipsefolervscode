db4eae2c04f6e6cd386b7d067250f9d4f074c526
GithubWoon <eo2458@naver.com>
2023-08-07 17:37:49 +0900

    0807

---
+++ day0807/src/fruit.java
@@ -0,0 +1,9 @@
+public class fruit {
+    public static void main(String[] args) {
+        // fruits 라는 String 배열 선언 후 초기화
+        String[] fruits = { "사과", "귤", "포도", "복숭아", "참외" };
+
+        // 배열 인덱스를 활용하여 귤을 출력
+        System.out.println(fruits[1]); // 두번째 인덱스 값을 불러옴
+    }
+}
