4071394ccb991f6f6111de7a1eb9c0be79013c6e
GithubWoon <eo2458@naver.com>
2023-08-07 17:39:25 +0900

    0807

--- day0807/src/study.java
+++ day0807/src/study.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class study {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("한 개의 숫자를 입력하세요: ");
