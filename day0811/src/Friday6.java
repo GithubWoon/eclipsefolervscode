2582ed5d1d0b76f162d9338f251ae6e535c90ab5
GithubWoon <eo2458@naver.com>
2023-08-11 15:17:58 +0900

    0811

---
+++ day0811/src/Friday6.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Friday6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 배열 크기 입력 받기
+        System.out.print("배열의 크기를 입력하세요: ");
+        int size = scanner.nextInt();
+        
+        // 정수형 배열 생성
+        int[] iArr = new int[size];
+        
+        // 가운데 위치 계산
+        int middleIndex = size / 2;
+        
+        // 가운데 위치에 값 입력 받기
+        System.out.print("가운데 위치에 넣을 정수 값을 입력하세요: ");
+        int middleValue = scanner.nextInt();
+        iArr[middleIndex] = middleValue;
+        
+        // 배열의 값을 출력하는 대신 배열 자체를 출력
+        System.out.print("배열의 값: [");
+        for (int i = 0; i < size; i++) {
+            System.out.print(iArr[i]);
+            if (i < size - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+        
+        scanner.close();
+    }
+}
