ca2e8c65e5798d17ccca2d008c3cf3dd10d11f9a
GithubWoon <eo2458@naver.com>
2023-08-18 17:51:23 +0900

    0818

--- day0818java/src/Main01.java
+++ day0818java/src/Main01.java
@@ -11,3 +11,7 @@ public class Main01 {
 }
 
 // 여기서는 DatabaseModule 인터페이스를 사용하여 OracleDatabaseModule 인스턴스를 생성합니다
+
+// jdk7 이후 상수필드, 추상메서드
+// jdk8 이후 상수필드, 추상메서드, default, static 메서드
+// jdk9 이후 private
